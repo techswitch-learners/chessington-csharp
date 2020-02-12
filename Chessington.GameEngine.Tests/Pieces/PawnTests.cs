1b9ca513f87dd911f3dc3aa9d58996b1d5ba65ab
Mike <Walker>
2020-02-12 23:36:14 +0000

    Add test to check pawns can move forwards.

--- Chessington.GameEngine.Tests/Pieces/PawnTests.cs
+++ Chessington.GameEngine.Tests/Pieces/PawnTests.cs
@@ -1,9 +1,34 @@
-﻿﻿using NUnit.Framework;
+﻿﻿using Chessington.GameEngine.Pieces;
+ using FluentAssertions;
+ using NUnit.Framework;
 
 namespace Chessington.GameEngine.Tests.Pieces
 {
     [TestFixture]
     public class PawnTests
     {
+        [Test]
+        public void WhitePawns_CanMoveOneSquareUp()
+        {
+            var board = new Board();
+            var pawn = new Pawn(Player.White);
+            board.AddPiece(Square.At(4, 0), pawn);
+
+            var moves = pawn.GetAvailableMoves(board);
+
+            moves.Should().Contain(Square.At(3, 0));
+        }
+
+        [Test]
+        public void BlackPawns_CanMoveOneSquareDown()
+        {
+            var board = new Board();
+            var pawn = new Pawn(Player.Black);
+            board.AddPiece(Square.At(4, 0), pawn);
+
+            var moves = pawn.GetAvailableMoves(board);
+
+            moves.Should().Contain(Square.At(5, 0));
+        }
     }
 }
