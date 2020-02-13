7972d119c1bfec345c782163afbfb6b1fd7a3c9e
Mike <Walker>
2020-02-13 00:32:26 +0000

    Add skeleton of a score counter.

---
+++ Chessington.GameEngine/ScoreCalculator.cs
@@ -0,0 +1,24 @@
+﻿namespace Chessington.GameEngine
+{
+    public class ScoreCalculator
+    {
+        private Board _board;
+
+        public ScoreCalculator(Board board)
+        {
+            _board = board;
+        }
+
+        public int GetWhiteScore()
+        {
+            // Should add up the value of all of the pieces that white has taken.
+            return 0;
+        }
+
+        public int GetBlackScore()
+        {
+            // Should add up the value of all of the pieces that black has taken.
+            return 0;
+        }
+    }
+}
